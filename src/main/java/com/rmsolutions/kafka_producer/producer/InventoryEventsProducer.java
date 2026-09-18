8559e79ff29b58db2088cce0bea80133e56a5741
Roger Rojas <byrogr.dev@gmail.com>
2026-09-17 22:30:21 -0500

    feat(producer): enhance InventoryEventsProducer to use ProducerRecord for sending events

--- src/main/java/com/rmsolutions/kafka_producer/producer/InventoryEventsProducer.java
+++ src/main/java/com/rmsolutions/kafka_producer/producer/InventoryEventsProducer.java
@@ -3,6 +3,7 @@ package com.rmsolutions.kafka_producer.producer;
 import com.rmsolutions.kafka_producer.model.dto.PurchaseEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -24,22 +25,27 @@ public class InventoryEventsProducer {
         try {
             var key = purchaseEvent.getOrderId();
             var value = objectMapper.writeValueAsString(purchaseEvent);
-            var completeStage = kafkaTemplate.send(topicName, key, value);
-            completeStage.whenComplete((result, ex) -> handleSendResult(key, value, result, ex));
+            ProducerRecord<String, String> record = new ProducerRecord<>(topicName, key, value);
+            var completableSendStage = kafkaTemplate.send(record);
+            completableSendStage.whenComplete((result, ex) ->
+                    handleSendResult(result.getProducerRecord(), result.getRecordMetadata().partition(), ex)
+            );
             log.info("** Purchase event sent to Kafka topic: {} **", value);
         } catch (Exception e) {
             log.error("** Error sending purchase event to Kafka: {} **", e.getMessage());
         }
     }
 
-    private void handleSendResult(String key, String value, SendResult<String, String> result, Throwable ex) {
+    private void handleSendResult(ProducerRecord<String, String> record, int partition, Throwable ex) {
+        String key = record.key(), value = record.value();
+        var partitionInfo = record.partition() != null ? record.partition() : partition;
         if (ex != null) {
             log.error("** Error sending message with key {} and value {}: {} **", key, value, ex.getMessage());
         } else {
             log.info("** Message sent successfully with key {} and value {} in partition {} **",
                     key,
                     value,
-                    result.getRecordMetadata().partition()
+                    partitionInfo
             );
         }
     }
